b2b981d2a03ea95eaf63c23407eccaf509e301ae
Osmay Moya Miranda <osmaymoya@gmail.com>
2021-03-27 18:53:46 -0400

    Implementada la paginacion, el filtro por sensor y el ordenamiento descendente

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
 
 from .serializers import MagnitudeSerializer
 from .models import Magnitude
@@ -30,5 +31,17 @@ class SensorViewSet(viewsets.ModelViewSet):
 
 
 class ValueLogViewSet(viewsets.ModelViewSet):
-    queryset = ValueLog.objects.all().order_by('time')
+    queryset = ValueLog.objects.all().order_by('-time')
     serializer_class = ValueLogSerializer
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = ValueLog.objects.all().order_by('-time')
+        sensor = self.request.query_params.get('sensor_id')
+        if sensor is not None:
+            queryset = queryset.filter(sensor__id=sensor)
+        return queryset
